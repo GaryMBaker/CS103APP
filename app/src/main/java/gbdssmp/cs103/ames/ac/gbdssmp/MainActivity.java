3909b92533be229dda83d72fbbf5771e335f0b6e
Gary Baker <garybaker@garys-macbook.local>
2018-06-06 21:31:25 +1200

    Gary's end of day upload

--- app/src/main/java/gbdssmp/cs103/ames/ac/gbdssmp/MainActivity.java
+++ app/src/main/java/gbdssmp/cs103/ames/ac/gbdssmp/MainActivity.java
@@ -1,7 +1,11 @@
 package gbdssmp.cs103.ames.ac.gbdssmp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //test version again
+
+        Button liveChat = (Button) findViewById(R.id.liveChat);
+        liveChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Message.class);
+                startActivity(intent);
+            }
+        });
+
+        Button forum = (Button) findViewById(R.id.forum);
+        forum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Forum.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
---
+++ app/src/main/java/gbdssmp/cs103/ames/ac/gbdssmp/Message.java
@@ -0,0 +1,60 @@
+package gbdssmp.cs103.ames.ac.gbdssmp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Message extends AppCompatActivity {
+
+    private ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+
+        lv = (ListView) findViewById(R.id.messagesList);
+
+        // Instanciating an array list (you don't need to do this,
+        // you already have yours).
+        List<String> your_array_list = new ArrayList<String>();
+
+        for (int i=0; i<10; i++) {
+            your_array_list.add("foo");
+            your_array_list.add("bar");
+        }
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                your_array_list );
+
+        lv.setAdapter(arrayAdapter);
+
+        // create action from onclick method to view each program
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position, long id) {
+
+                // print out title of program
+                TextView tvItemClicked = (TextView) itemClicked;
+                String strItemClicked = tvItemClicked.getText().toString();
+
+                // Explicitly use intent to open new Activity
+//                Intent intent = new Intent(listview_Activity.this, ProgramInfo_Activity.class);
+//                intent.putExtra("program_name", programs_array[position]);
+//                startActivity(intent);
+            }
+        });
+    }
+}
